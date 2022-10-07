1087dc15babb5e264cbdde96ddb93782d36d87ad
tungUAS <tung.tt133@gmail.com>
2022-10-07 16:40:17 +0200

    find all blog posts by user id

--- ServerSide/controller/user.controller.js
+++ ServerSide/controller/user.controller.js
@@ -1,6 +1,8 @@
+const { INTEGER } = require('sequelize');
 const db = require('../models')
 const Category = db.category;
 const Post = db.post;
+const User = db.user;
 const Op = db.Sequelize.Op;
 
 exports.addBlogPost = async (req,res) => {
@@ -29,18 +31,38 @@ exports.addBlogPost = async (req,res) => {
     }
 }
 
-exports.getAllBlogPosts = async (req,res) => {
+exports.getAllBlogPostsByUserID = async (req,res) => {
     try{
+
+    const page = req.query.page; // 1 2 3 4
+    const limit = req.query.limit; // 10 
+
+    const passedItems = (page - 1)*limit; // 0 10 20 30
+
         const posts = await Post.findAll({
+            attributes:['post_id','post_title','post_content'],
             where:{
-                userUserId:{
-                    [Op.eq]:req.user_id
-                }
-            }
+                userUserId:req.user_id
+            },
+            
+            include:[{
+                model:Category,
+                through:{
+                    attributes:[]
+                },
+                order:['post_id','ASC']
+            }],
+            offset:parseInt(passedItems),
+            limit:parseInt(limit),
+            subQuery:false
         })
 
         return res.json(posts)
     }catch(error){
         console.log(error);
     }
+}
+
+exports.getAllBlogPostByUserIdAndCategory = async (req,res) => {
+    
 }
